101e7f62b208019556ce1041e973cf6868fca20d
aKoranteng <akoranteng@msn.com>
2024-03-22 15:05:06 -0400

    Add project files.

---
+++ DxBlazorApplication2.E2E.Tests/Tests.cs
@@ -0,0 +1,39 @@
+﻿using DevExpress.EasyTest.Framework;
+using Xunit;
+
+[assembly: CollectionBehavior(DisableTestParallelization = true)]
+
+// To run functional tests for ASP.NET Web Forms and ASP.NET Core Blazor XAF Applications,
+// install browser drivers: https://www.selenium.dev/documentation/getting_started/installing_browser_drivers/.
+//
+// -For Google Chrome: download "chromedriver.exe" from https://chromedriver.chromium.org/downloads.
+// -For Microsoft Edge: download "msedgedriver.exe" from https://developer.microsoft.com/en-us/microsoft-edge/tools/webdriver/.
+//
+// Selenium requires a path to the downloaded driver. Add a folder with the driver to the system's PATH variable.
+//
+// Refer to the following article for more information: https://docs.devexpress.com/eXpressAppFramework/403852/
+
+namespace DxBlazorApplication2.Module.E2E.Tests;
+
+public class DxBlazorApplication2Tests : IDisposable {
+    const string BlazorAppName = "DxBlazorApplication2Blazor";
+    const string AppDBName = "DxBlazorApplication2";
+    EasyTestFixtureContext FixtureContext { get; } = new EasyTestFixtureContext();
+
+	public DxBlazorApplication2Tests() {
+        FixtureContext.RegisterApplications(
+            new BlazorApplicationOptions(BlazorAppName, string.Format(@"{0}\..\..\..\..\DxBlazorApplication2.Blazor.Server", Environment.CurrentDirectory))
+        );
+        FixtureContext.RegisterDatabases(new DatabaseOptions(AppDBName, "DxBlazorApplication2EasyTest", server: @"(localdb)\mssqllocaldb"));	           
+	}
+    public void Dispose() {
+        FixtureContext.CloseRunningApplications();
+    }
+    [Theory]
+    [InlineData(BlazorAppName)]
+    public void TestBlazorApp(string applicationName) {
+        FixtureContext.DropDB(AppDBName);
+        var appContext = FixtureContext.CreateApplicationContext(applicationName);
+        appContext.RunApplication();
+    }
+}
